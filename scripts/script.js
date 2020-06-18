4964ddc243412f64da1d86364167ecc7e1e13890
Constantin Ginga <costelgn@gmail.com>
2020-06-18 18:37:22 +0300

    Add full game functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,38 @@
 const gameGrid = document.querySelector('#game-grid'),
     gameCells = document.querySelectorAll('.game-cell');
 
+let gameState = true;
+
 
 
 
 // TO-DO
-// keep track of winner name
-// check for a tie
-// announce winner immediately
+// X red, O blue (both dark)
+// choose font (for marks and for text different)
+// first show form, then after hitting play button, show board
+// two forms side by side: Name, mark;
+// when game finished, announce winner and show 'play again' button
+// when click 'play again', reset board with animation and hide button and winner announcement
+// add transition animations
+// make it responsive (both form and board)
+// make background math paper
+// change all instances of private variables with underscore
+// set gamestate to true when user clicks play (reset the temp value of true above)
+
+
 const gameBoard = (() => {
     const board = [];
     const update = (cell, i) => {
-        // fix bug
-        if (board.length !== 9 && !board.includes(undefined)) {
-            if (gameLogic.checkWinner()) console.log('You won!')
-            else if (cell.innerHTML == '') {
-                cell.innerHTML = gameLogic.makeTurn();
-                board[i] = cell.innerHTML;
-            }
-        } else {
-            console.log(`It's a tie`);
+        // fill board array and html divs
+        if (cell.innerHTML === '') {
+            board[i] = gameLogic.makeTurn();
+            cell.innerHTML = `<p>${board[i]}</p>`
         }
+        let winner = gameLogic.checkWinner();
+        // if there's a winner, declare the winner
+        if (winner) displayController.announceWinner(winner)
+        // if the board is full, declared tie
+        else if (board.length === 9 && !board.includes(undefined)) displayController.announceWinner(`You tied.`);
     }
     return {board, update};
 })();
@@ -49,36 +61,61 @@ const gameLogic = (() => {
     }
 
     const checkWinner = () => {
-        let position;
+        let position, winner;
+        // check for the right pattern
         if (gameBoard.board[4] !== undefined) {
             position = gameBoard.board[4];
             if (gameBoard.board[0] === position && gameBoard.board[8] === position || 
                 gameBoard.board[2] === position && gameBoard.board[6] === position ||
                 gameBoard.board[1] === position && gameBoard.board[7] === position ||
-                gameBoard.board[3] === position && gameBoard.board[5] === position) return true;
+                gameBoard.board[3] === position && gameBoard.board[5] === position) winner = position;
         }
         if (gameBoard.board[0] !== undefined) {
             position = gameBoard.board[0];
             if (gameBoard.board[1] === position && gameBoard.board[2] === position ||
-                gameBoard.board[3] === position && gameBoard.board[6] === position) return true;
+                gameBoard.board[3] === position && gameBoard.board[6] === position) winner = position;
         }
         if (gameBoard.board[8] !== undefined) {
             position = gameBoard.board[8];
             if (gameBoard.board[7] === position && gameBoard.board[6] === position ||
-                gameBoard.board[5] === position && gameBoard.board[2] === position) return true;
+                gameBoard.board[5] === position && gameBoard.board[2] === position) winner = position;
         }
+
+        // return winning player's name
+        if (winner === playerOne.mark) return playerOne.name
+        else if (winner === playerTwo.mark) return playerTwo.name;
+        // return false by default
         return false;
     }
     
-    return {makeTurn, checkWinner}
+    return {makeTurn, checkWinner};
+})();
+
+const displayController = (() => {
+
+    // show winner and stop game
+    const announceWinner = (winner) => {
+        gameState = false;
+        const winnerPara = document.querySelector('#winner');
+        winnerPara.innerHTML = `Congrats ${winner}, you won!`;
+        console.log(winner);
+    }
+
+    const toggleBoard = () => {
+        // toggle between boards and set game status
+    }
+
+    return {announceWinner};
 })();
 
 
 gameCells.forEach((cell, i) => {
     cell.addEventListener('click', e => {
-        gameBoard.update(cell, i);
+        if (gameState) gameBoard.update(cell, i);
     });
 });
+
+
 
 
 
